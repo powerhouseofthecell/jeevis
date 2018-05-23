a67c4b69466b9e2e98344a32cf25337a7b7e5810
powerhouseofthecell <nicholas_wong@college.harvard.edu>
2018-05-23 12:16:59 -0700

    oops fixed the static root

--- jeevis/settings.py
+++ jeevis/settings.py
@@ -127,3 +127,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
